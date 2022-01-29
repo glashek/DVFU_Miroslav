37d56e1cfe2c4eab1ebcf3aa447b67f11e3696b8
glashek <glashek@mail.ru>
2022-01-29 10:20:07 +1000

    msg

---
+++ src/ru/dvfu/mrcpk/java01/example11/easyclass/Point2D.java
@@ -0,0 +1,20 @@
+package ru.dvfu.mrcpk.java01.example11.easyclass;
+
+public class Point2D {
+    private int x;
+    private int y;
+    public Point2D(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+    public Point2D(int size) {
+        x = size;
+        y = size;
+    }
+    public int getX() {
+        return x;
+    }
+    public int getY() {
+        return y;
+    }
+}
